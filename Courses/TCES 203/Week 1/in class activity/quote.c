55003c660efc7a611a7ff3b23d29e64c8e4df505
daniilnahl <daniil.nahl@protonmail.com>
2025-09-24 10:10:47 -0700

    Add notes from TCES203 lecture
    Added notes and practice problems from lecture

---
+++ Courses/TCES 203/Week 1/in class activity/quote.c
@@ -0,0 +1,18 @@
+/****
+Author: Daniil Nahliuk
+
+Task: 
+Create quote.c that prints two lines:
+   1. To C, or not to C: that is the question.
+   2.  --Shakespeare
+    
+   Constraints: Must include a file header block comment (/****|)
+    and at least one line comment (// ...). Use one printf with two \n or two separate printf calls.
+ */
+
+ #include <stdio.h>
+ int main() {
+    printf("To C, or not to C: that is the question.\n\n--Shakespeare\n"); //printf command
+
+    return 0;
+ }
---
+++ Courses/TCES 203/Week 1/notes week1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    float i = 0.2121f; //add f at the end of float to prevent compiler warning messages
+    return 0;
+}
