70a4da851e22466da84cb8269e96bcf771df79a9
daniilnahl <daniil.nahl@protonmail.com>
2025-10-02 14:05:48 -0700

    Add notes and homework

---
+++ Courses/TCES 203/Week 2/io_lab.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main(){    
+    printf("TWO LINE HEADING\n\"Item\"\t\t\\\t\"Value\"\t\t\\\t\"Notes\"\n\n");
+    int num_1 = 12345678;
+    float num_2 = 1.2345678123456789;
+    printf("Shortened Float using g specifier: %g\n",num_2);
+    printf("Float(with precision): %.15f\n",num_2);
+    printf("Float(with width): %3.f\n",num_2);
+    printf("Float(without specified width and precision): %f\n",num_2);
+    printf("Int: %d\n",num_1);
+
+    int date_1;
+    scanf("")
+
+    return 0;
+}
