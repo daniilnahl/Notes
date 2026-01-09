9ed3273cbdc8a88da6653125889b5116a32e030e
daniilnahl <daniil.nahl@protonmail.com>
2026-01-09 15:22:49 -0800

    Add notes on creating a NN with basic process on how to train one

--- Artificial-Intelligence/Deep Learning/PyTorch/main.py
+++ Artificial-Intelligence/Deep Learning/PyTorch/main.py
@@ -1,7 +1,53 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
 from torchvision.models import resnet18, ResNet18_Weights
 import numpy as np
 
+import pytorch_practice1 as pp1 
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # 1 input image channel, 6 output channels, 5x5 square convolution
+        #kernel
+        self.conv1 = nn.Conv2d(1, 6, 5) #NN layer? 1 input to 6 outputs
+        self.conv2 = nn.Conv2d(6, 16, 5) ##NN layer? 6 inputs to 16 outputs
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)  # 5*5 from image dimension
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+    
+    def forward(self, input):
+        # Convolution layer C1: 1 input image channel, 6 output channels,
+        # 5x5 square convolution, it uses RELU activation function, and
+        # outputs a Tensor with size (N, 6, 28, 28), where N is the size of the batch
+        c1 = F.relu(self.conv1(input))
+        # Subsampling layer S2: 2x2 grid, purely functional,
+        # this layer does not have any parameter, and outputs a (N, 6, 14, 14) Tensor
+        s2 = F.max_pool2d(c1, (2, 2))
+        # Convolution layer C3: 6 input channels, 16 output channels,
+        # 5x5 square convolution, it uses RELU activation function, and
+        # outputs a (N, 16, 10, 10) Tensor
+        c3 = F.relu(self.conv2(s2))
+        # Subsampling layer S4: 2x2 grid, purely functional,
+        # this layer does not have any parameter, and outputs a (N, 16, 5, 5) Tensor
+        s4 = F.max_pool2d(c3, 2)
+        # Flatten operation: purely functional, outputs a (N, 400) Tensor
+        s4 = torch.flatten(s4, 1)
+        # Fully connected layer F5: (N, 400) Tensor input,
+        # and outputs a (N, 120) Tensor, it uses RELU activation function
+        f5 = F.relu(self.fc1(s4))
+        # Fully connected layer F6: (N, 120) Tensor input,
+        # and outputs a (N, 84) Tensor, it uses RELU activation function
+        f6 = F.relu(self.fc2(f5))
+        # Fully connected layer OUTPUT: (N, 84) Tensor input, and
+        # outputs a (N, 10) Tensor
+        output = self.fc3(f6)
+        return output      
+ 
 
 def tensors():
     #1. Tensor Initialization aka ways to create them
@@ -114,9 +160,81 @@ def autograd():
     #initiate gradient descent and optimizer adjusts each parameter by its gradient stored with '.grad'
     optim.step()
 
+def nn_example():
+    nn_ex = Net()
+    
+    ##EXAMPLE 1
+    #shows learnable parameters
+    params = list(nn_ex.parameters()) 
+    print(len(params))
+    print(params[0].size()) #conv1's weights
+
+
+    ##EXAMPLE 2
+    #random 32x32 input - process it
+    input = torch.randn(1, 1, 32, 32)
+    out = nn_ex(input)
+    #print(out)
+
+    #zero gradient buffers of all parameters and back prop. with random gradients
+    nn_ex.zero_grad()
+    out.backward(torch.randn(1, 10))
+    #print(out)
+
+
+    #ACTUAL PRACTICE
+    #1 declare nn, feed it input, compute loss, backpropagate, update the weights, feed input again 
+    ##declare nn
+    nnn = Net()
+    ##feed input
+    input1 = torch.randn(1, 1, 32, 32)
+    out1 = nnn(input1)
+
+    target = torch.randn(10) # a dummy target, for example
+    target = target.view(1, -1) # make it the same shape as output
+
+    ##compute loss
+    criterion = nn.MSELoss()
+    loss = criterion(out1, target) #compare the two?
+    print("The loss is\n")
+    print(loss)
+
+    ##graph of computitons in backward direction
+    print(loss.grad_fn)  # MSELoss
+    print(loss.grad_fn.next_functions[0][0])  # Linear
+    print(loss.grad_fn.next_functions[0][0].next_functions[0][0])  # ReLU
+
+
+    ##back prop.
+    nnn.zero_grad() # zeroes the gradient buffers of all parameters
+    print('conv1.bias.grad before backward')
+    print(nnn.conv1.bias.grad)
+
+    loss.backward()
+    print('conv1.bias.grad after backward')
+    print(nnn.conv1.bias.grad)
+
+    ##adjusting weights - 2 methods
+    ### simplest update rule used in practice is the Stochastic Gradient Descent (SGD)  -> weight = weight - learning_rate * gradient. Don't do that use the sample code below (faster)
+    learning_rate = 0.01
+    for f in nnn.parameters():
+        f.data.sub_(f.grad.data * learning_rate)
+
+    ###method 2: using torch.optim that implements various methods of adjusting weight (using SGD,Nesterov-SGD, Adam, RMSProp, etc )
+    # create your optimizer
+    optimizer = optim.SGD(nnn.parameters(), lr=0.01)
+
+    # in your training loop:
+    optimizer.zero_grad()   # zero the gradient buffers
+    output = nnn(input)
+    loss = criterion(output, target)
+    loss.backward()
+    optimizer.step()    # Does the update
+
 def main():
+
     print("PyTorch version:", torch.__version__)  
-    autograd()
+    nn_example()
 
 if __name__ == "__main__":
     main()
--- Artificial-Intelligence/Deep Learning/PyTorch/pytorch-notes.md
+++ Artificial-Intelligence/Deep Learning/PyTorch/pytorch-notes.md
@@ -8,7 +8,41 @@ Open Source Deep Learning Framework. Its integrated with Python.
 When working with NNs in PyTorch NNs are a collection of nested functions that are executed on some input data. These functions are defined by:
 - parameters (weights and biases).
 
+**torch.autograd** is Pytorch's differentiaion engine that powers neural network training.
 
 Training of NNs happens in this way:
 1. **Forward propagation**: NN makes its best guess about the correct output to achieve this it runs the input data through each of its function (neurons) aka making a guess.
 2. **Backward propagation**: NN adjusts its parameters proportionate to the error in its guess. *How does it do it?* It does it by traversing the NN backwards from the output layer, collecting the derivatives of the error in respect to the parameters functions (gradients) and optimizing using **gradient descent**.
+
+# How to make a NN in PyTorch?
+### 1. Define a NN that has some learnable parameters or weights
+Define the NN with its layers. Then define forward function and the backward function is automatically defined. 
+Note: you can showcase learnable parameters by, 
+```
+#shows learnable parameters
+params = list(nn_ex.parameters()) 
+print(len(params))
+print(params[0].size()) #conv1's weights
+```
+### 2. Iterate over a dataset of inputs
+### 3. Process input through the netwrok
+### 4. Compute the loss (how far the output is from being correct)
+There is a loss function which takes the (output, target) pair of inputs, and computes a value that estimates how far away the output is from the target. 
+**There are multiple types of loss functions** all are within nn package:
+1.  nn.MSELoss -> computes mean-squared error between the outputs and the target.
+2. 
+3. 
+
+### 5. Propagate gradients back into the network's parameters
+### 6. Update the weights of the network, typically using a simple update rule
+Simple update rule: ```weight = weight - learning_rate * gradient```
+### 7. Rinse and repeat step 3-6 for N amount of times.
+
+
+
+# Vocab
+- **torch.Tensor**: A multi-dimensional array with support for autograd operations like backward(). Also holds the gradient w.r.t. the tensor.
+- **nn.Module**: Neural network module. Convenient way of encapsulating parameters, with helpers for moving them to GPU, exporting, loading, etc.
+- **nn.Parameters**: A kind of Tensor, that is automatically registered as a parameter when assigned as an attribute to a Module.
+- **autograd.Parameter**: Implements forward and backward definitions of an autograd operation. Every Tensor operation creates at least a single Function node that connects to functions that created a Tensor and encodes its history.
+- ****:
