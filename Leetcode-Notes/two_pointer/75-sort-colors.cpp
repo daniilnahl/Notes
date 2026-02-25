100af1f8ba275da34d2588510b0b5275167d8351
daniilnahl <daniil.nahl@protonmail.com>
2026-02-24 17:19:51 -0800

    Add Dutch National Flag one-pass solution
    Introduce a second, more efficient implementation of sortColors using the Dutch National Flag algorithm (one-pass, in-place). Also add comments documenting the original counting-based (4-pass) approach and the new one-pass method for clarity.

--- Leetcode-Notes/two_pointer/75-sort-colors.cpp
+++ Leetcode-Notes/two_pointer/75-sort-colors.cpp
@@ -1,6 +1,8 @@
 #include <string>
 using namespace std;
 
+
+//1st approach - works but inefficient - 4 passes!
     void sortColors(vector<int>& nums) {
         //sort the array such that adjacent num/colors are next to each other 
         //0 is red
@@ -22,7 +24,22 @@ using namespace std;
         for (int i = 0; i < blue; i++) nums.push_back(2);
     }
 
-
+//2nd approach - the most efficient one - only 1 for loop pass - Dutch National Flag algorithm - niche ass algorithm
+    void sortColors(vector<int>& nums) {
+        int low = 0, mid = 0, high = nums.size()-1;
+        while (mid <= high){
+            if(num[mid] == 0){
+                swap(num[low], nums[mid]);
+                low++;
+                mid++;
+            } else if(nums[mid] == 1){
+                mid++;
+            } else{
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
 /****** 
  * WHAT WAS CORE IDEA?
  The core idea was to count the occurrences of each color, then empty out the vector, afterwards fill it up with 0s, 1s and 2s based on the counts within first loop.
